cb2ef63ab5fc9946877feacb776bd0aed92f6bc3
Elton Brayner <eltonbrayner1@gmail.com>
2021-06-13 22:24:51 -0300

    :construction: left pass on any tests

---
+++ readme.md
@@ -0,0 +1,35 @@
+# Ignite Challenge
+
+## Desafio 03
+
+## Tópicos
+
+[ ] Adicionar um novo produto ao carrinho;
+[ ] Remover um produto do carrinho;
+[ ] Alterar a quantidade de um produto no carrinho;
+[ ] Cálculo dos preços sub-total e total do carrinho;
+[ ] Validação de estoque;
+[ ] Exibição de mensagens de erro;
+[ ] Entre outros.
+
+## Objetivos secundários
+
+[ ] Fake API com JSON Server;
+[ ] Preservar dados do carrinho com localStorage API;
+[ ] Mostrar erros com toastify.
+
+## API Server
+
+URL Principal: http://localhost:3333/
+Rotas:
+
+- /stock
+- /products
+
+## Localstorage
+
+Setando informação:
+localStorage.setItem('@RocketShoes:cart', cart)
+
+Recebendo informação:
+localStorage.getItem('@RocketShoes:cart')
--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -23,28 +23,73 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem('@RocketShoes:cart')
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      //Novo array com os valores de cart
+      const updatedCart = [...cart];
+      //Verifico se o produto já existe no carrinho através do metodo FIND 
+      //Caso exista ele retorna o elemento encontrado e se não existir retorna undefined
+      const productExists = updatedCart.find(product => product.id === productId);
+
+      const stock = await api.get(`stock/${productId}`) //retorna o estoque do produto (id e amount) da loja
+      const stockAmount = stock.data.amount; //estoque disponivel na loja
+
+      //Caso já exista no carrinho retorna a quantidade que está no carrinho 
+      const currentAmount = productExists ? productExists.amount : 0;
+      const amount = currentAmount + 1; //quantidade desejada do cliente (intuito de verificar se passou do limite)
+
+      if(amount > stockAmount){
+        toast.error('Quantidade solicitada fora de estoque')
+        return
+      }
+
+      if(productExists){
+        productExists.amount = amount;
+      }else {
+        const product = await api.get(`/products/${productId}`)
+
+        //É necessario adicionar mais uma propriedade ao objeto porque o PRODUCT faz referencia lista de produtos
+        //E essa lista não tem a quantidade. No carrinho é preciso, além das caracteristicas da lista, a quantidade
+        const newProduct = {
+          ...product.data,
+          amount: 1
+        }
+        updatedCart.push(newProduct);
+      }
+
+      setCart(updatedCart);
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart))
     } catch {
-      // TODO
+      toast.error('Erro na adição do produto')
     }
   };
 
   const removeProduct = (productId: number) => {
     try {
-      // TODO
+      const updatedCart = [...cart]
+      //Find index para verificar qual é o index do product na Array e poder utilizar o slice para remover
+      const productIndex = updatedCart.findIndex(product => product.id === productId)
+
+      //Caso o findIndex não encontre ele retorna -1
+      //Caso encontre retorna o indice do array (que inicia em zero)
+      if(productIndex >= 0){
+        updatedCart.splice(productIndex, 1) //começando do indice encontrado e elementos a ser apagados
+        setCart(updatedCart)
+        localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart))
+      }else {
+        throw Error(); //forçar a cair no catch
+      }
     } catch {
-      // TODO
+      toast.error('Erro na remoção do produto');
     }
   };
 
